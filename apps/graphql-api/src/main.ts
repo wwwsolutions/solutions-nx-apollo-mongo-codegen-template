b4cac63e51a23cd0f39d1e368ca4b6e7f52aebf2
Domagoj-Mario Mendas <42439126+wwwsolutions@users.noreply.github.com>
2021-03-23 09:26:56 +0100

    added environment variables support

--- apps/graphql-api/src/main.ts
+++ apps/graphql-api/src/main.ts
@@ -3,6 +3,8 @@ import { ApolloServer } from 'apollo-server';
 import resolvers from './resolvers';
 import typeDefs from './type-defs';
 
+console.log(process.env.FOO);
+
 const server = new ApolloServer({ resolvers, typeDefs });
 
 server.listen().then(({ url }) => console.log(`Server ready at ${url}. `));
