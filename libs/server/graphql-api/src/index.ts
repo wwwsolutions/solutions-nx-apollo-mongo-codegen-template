130cd9314d4d664d2659791aab7654b3e9aca732
Domagoj-Mario Mendas <42439126+wwwsolutions@users.noreply.github.com>
2021-03-24 20:38:37 +0100

    added merge resolvers functionality

--- libs/server/graphql-api/src/index.ts
+++ libs/server/graphql-api/src/index.ts
@@ -1,9 +1,18 @@
-// TYPE DEFINITIONS
+// MERGED TYPE DEFINITIONS
 import { mergeTypeDefs } from '@graphql-tools/merge';
 import * as userTypeDefs from './lib/user/user.type-defs.graphql';
 import * as postTypeDefs from './lib/post/post.type-defs.graphql';
 
 export const typeDefs = mergeTypeDefs([userTypeDefs, postTypeDefs]);
 
-// RESOLVERS
-export * from './lib/resolvers';
+// MERGED RESOLVERS
+import merge from 'lodash/merge';
+import { graphqlScalarsResolver } from './lib/custom-resolver';
+import { postResolver } from './lib/post/post-resolver';
+import { userResolver } from './lib/user/user-resolver';
+
+export const resolvers = merge(
+  graphqlScalarsResolver,
+  postResolver,
+  userResolver
+);
